77c79b2e355fc848700a1f897f9efb60d64c2c2e
Miggie Garcia <miggiegarciadev@gmail.com>
2021-06-17 20:00:00 -0600

    daily codewars post in js

--- june2021.js
+++ june2021.js
@@ -1,21 +1,37 @@
 //codewar
 
 // 1 June
+
 // 2 June
+
 // 3 June
+
 // 4 June
+
 // 5 June ****
+
 // 6 June
+
 // 7 June
+
 // 8 June
+
 // 9 June
+
 // 10 June
+
 // 11 June
+
 // 12 June
+
 // 13 June **
+
 // 14 June
+
 // 15 June
+
 // 16 June
+
 // 17 June
 // 18 June
 // 19 June
